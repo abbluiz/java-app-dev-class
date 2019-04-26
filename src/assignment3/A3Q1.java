52d4cfa66ffa36ee215b9f0d41b0170f240490f1
abbluiz <unt@labb.email>
2019-04-26 17:02:08 -0400

    Add initial assignment 3 code

---
+++ src/assignment3/A3Q1.java
@@ -0,0 +1,18 @@
+package assignment3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class A3Q1 extends Application {
+	@Override
+	// Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		// TO DO: Write your JavaFX application here ..
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
+
+// TO DO: Write any other class here, if it is required ...
---
+++ src/assignment3/A3Q2.java
@@ -0,0 +1,12 @@
+package assignment3;
+
+public class A3Q2 {
+	public static void main(String[] args) {
+		// TO DO: Write your test application here ...
+	}
+
+	public static int gcd(int m, int n) {
+		// TO DO: Write your recursive method here ...
+		return 0; // TO DO: Modify the return statement accordingly ...
+	}
+}

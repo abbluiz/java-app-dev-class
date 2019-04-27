f9b40f8c1646de5b4c9151c6d98a53afc3e6d8e2
abbluiz <unt@labb.email>
2019-04-26 21:25:24 -0400

    Create simple gcd calculator

--- src/assignment3/A3Q2.java
+++ src/assignment3/A3Q2.java
@@ -1,12 +1,29 @@
 package assignment3;
 
+import java.util.Scanner;
+
 public class A3Q2 {
 	public static void main(String[] args) {
 		// TO DO: Write your test application here ...
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter two integers: ");
+
+		int m = input.nextInt();
+		int n = input.nextInt();
+
+		System.out.println("Their GCD is " + gcd(m, n));
+
 	}
 
 	public static int gcd(int m, int n) {
 		// TO DO: Write your recursive method here ...
-		return 0; // TO DO: Modify the return statement accordingly ...
+
+		if (m % n == 0)
+			return n;
+		else
+			return gcd(n, m % n); // TO DO: Modify the return statement accordingly ...
+
 	}
 }
